76f6e2ff3b55c3fdd5f2d1b818ae04ed25edfc70
mdtang <mdtang@umich.edu>
2016-11-04 22:03:40 -0400

    Part B code complete

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -10,3 +10,32 @@
 
 # Deliverables
 # Make sure the new page is uploaded to your GitHub account.
+
+import urllib.request, urllib.parse, urllib.error
+import re
+from bs4 import BeautifulSoup
+
+# Creating index.html file
+fopen = open('index.html', 'w')
+
+url = 'http://collemc.people.si.umich.edu/data/bshw3StarterFile.html'
+html = urllib.request.urlopen(url).read().decode()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Searching for text that includes the word student
+students = soup.find_all(text=re.compile('student'))
+
+
+for line in students:
+	replace = line.replace('student', 'AMAZING student')
+	line.replace_with(replace)
+
+for img in soup.find_all('img'):
+	if img['src'] == 'logo2.png':
+	    img['src'] = 'media/logo.png'
+	else:
+	    img['src'] = 'media/image.jpg' 
+
+prettify = soup.prettify()
+fopen.write(prettify)
+
